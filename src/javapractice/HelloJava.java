ab7e20ac6a3a1afbf5516c4584805f9a11cc5cde
krtkumar082 <kkumar2_be16@thapar.edu>
2020-09-21 22:44:24 +0530

    kk

--- src/javapractice/HelloJava.java
+++ src/javapractice/HelloJava.java
@@ -4,10 +4,10 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Hello Linux");
-<<<<<<< HEAD
+
 		System.out.println("Hello local");
-=======
+
 		System.out.println("Hello Remotes");
->>>>>>> 14d3319b817138ae2b7ac08e3b69ff227f811f12
+
 	}
 }
