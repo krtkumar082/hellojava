14d3319b817138ae2b7ac08e3b69ff227f811f12
krtkumar082 <71620970+krtkumar082@users.noreply.github.com>
2020-09-21 22:36:35 +0530

    commit

--- src/javapractice/HelloJava.java
+++ src/javapractice/HelloJava.java
@@ -4,6 +4,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Hello Linux");
-		System.out.println("Hello Remote");
+		System.out.println("Hello Remotes");
 	}
 }
